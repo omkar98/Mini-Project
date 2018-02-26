dcf13ac605174b5a70717d0ff0648b3d0027d808
Omkar Deshpande <edu.omkar@gmail.com>
2018-02-26 17:25:51 +0530

    MINI PROJECT Day-1,day2 modules

---
+++ DAY-1.C
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<graphics.h>
+#include<stdlib.h>
+#include<process.h>
+#include<conio.h>
+
+#define size 10
+
+int midx,midy;
+
+ void display()
+ {
+  setfillstyle(USER_FILL,WHITE);
+  floodfill(2,2,WHITE);
+  rectangle(0,0,getmaxx(),getmaxy());
+
+
+  settextstyle(0,0,1);
+  setcolor(6);
+  outtextxy(midx+30,midy-100,"Index");
+  outtextxy(midx+105,midy+100,"Bottom");
+  outtextxy(midx+55,midy+90,"0");
+  outtextxy(midx+55,midy+70,"1");
+  outtextxy(midx+55,midy+50,"2");
+  outtextxy(midx+55,midy+30,"3");
+  outtextxy(midx+55,midy+10,"4");
+  outtextxy(midx+55,midy-10,"5");
+  outtextxy(midx+55,midy-30,"6");
+  outtextxy(midx+55,midy-50,"7");
+  outtextxy(midx+55,midy-70,"8");
+  outtextxy(midx+55,midy-90,"9");
+  line(midx+100,midy+100,midx+100,midy-100);
+  line(midx+70,midy+100,midx+70,midy-100);
+  line(midx+100,midy+100,midx+70,midy+100);
+
+  line(midx+100,midy+80,midx+70,midy+80);
+  line(midx+100,midy+60,midx+70,midy+60);
+  line(midx+100,midy+40,midx+70,midy+40);
+  line(midx+100,midy+20,midx+70,midy+20);
+  line(midx+100,midy,midx+70,midy);
+  line(midx+100,midy-20,midx+70,midy-20);
+  line(midx+100,midy-40,midx+70,midy-40);
+  line(midx+100,midy-60,midx+70,midy-60);
+  line(midx+100,midy-80,midx+70,midy-80);
+}
+
+
+int main()
+{
+ int ch,i;
+ int gdriver = DETECT, gmode, errorcode;
+
+ initgraph(&gdriver, &gmode, "c:\\turboc3\\BGI");
+
+ midx = getmaxx() / 2;
+ midy = getmaxy() / 2;
+ setcolor(BLACK);
+ display();
+ getch();
+ clrscr();
+ return 0;
+}
+
